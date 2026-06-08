30b7a42fb2b27634cbe06f2249c7dc1e26945af7
Douglas-M-Miranda <douglamacedo@gmail.com>
2026-06-08 11:29:15 -0300

    project completion

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -30,3 +30,35 @@ function ProductDetails({data}) {
 }
 
 export default ProductDetails;
+
+//backup temporario
+{/* <section className={styles.containerProjets}>
+            <article className={styles.containerProjetsItems}>
+                
+                <div className={styles.projectDetails}>
+                    <div className={styles.projectImage}>
+                        <img src={imagem1} alt="" />
+                    </div>
+
+                    <div className={styles.projectDescription}>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, consequatur. 
+                        </p>
+                    </div>
+                </div>
+--------------------------------------------------------------------------
+                <div className={styles.projectDetails}>
+
+                    <div className={styles.projectImage}>
+                        <img src={imagem1} alt="" />
+                    </div>
+
+                    <div className={styles.projectDescription}>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, consequatur. 
+                        </p>
+                    </div>
+                </div>
+
+            </article>
+    </section> */}
